dd6d380deb60940174eae7ee13696c86816ce7b3
Matt <mcollins24@hanoverstudents.org>
2022-12-02 00:42:02 -0500

    finishedday2

--- AoC-2022/d2.py
+++ AoC-2022/d2.py
@@ -24,3 +24,28 @@ def part1():
     print("Part 1: " + str(totalScore))
 part1()
 
+def part2():
+    file = open('d2.dat', 'r')
+    lines = file.readlines()
+    totalScore = 0
+    for line in lines:
+        if(line.strip() == 'A X'):
+            totalScore += 3
+        elif(line.strip() == 'A Y'):
+            totalScore += 4
+        elif(line.strip() == 'A Z'):
+            totalScore += 8
+        elif(line.strip() == 'B X'):
+            totalScore += 1
+        elif(line.strip() == 'B Y'):
+            totalScore += 5
+        elif(line.strip() == 'B Z'):
+            totalScore += 9
+        elif(line.strip() == 'C X'):
+            totalScore += 2
+        elif(line.strip() == 'C Y'):
+            totalScore += 6
+        elif(line.strip() == 'C Z'):
+            totalScore += 7
+    print("Part 2: " + str(totalScore))
+part2()
