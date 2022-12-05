727a67f5ecd3e792e0b60bf4b7537a455ebd9b3f
Matt Collins <mcollins24@hanoverstudents.org>
2022-12-05 12:48:23 -0500

    kutryjthh

--- AoC-2022/d3.py
+++ AoC-2022/d3.py
@@ -1,3 +1,18 @@
 def part1():
-    file = open('d2.dat', 'r')
+    file = open('d3.dat', 'r')
     lines = file.readlines()
+    compartmentSimilarities = []
+    for line in lines:
+
+        for j in range(len(line)/2):
+            
+            for k in range(len(line)/2,len(line)/2):
+
+                if(line[j] == line[k]):
+                    
+                    compartmentSimilarities.push(line[j])
+
+    print(compartmentSimilarities)
+#cd .\AoC-2022\   
+#python3 d3.py
+part1()
