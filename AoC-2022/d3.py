af4a58df61fb30a2bdaa1f30c61ba5c170e83044
Matt Collins <mcollins24@hanoverstudents.org>
2022-12-06 17:58:55 -0500

    jkdkfgjrn

--- AoC-2022/d3.py
+++ AoC-2022/d3.py
@@ -1,20 +1,37 @@
+# The following helper function was 'borrowed' from bciccolo (https://github.com/bciccolo).
+
+def getPriority(letter):
+    priority = ord(letter)
+
+    if (priority > 96): # lowercase
+        priority -= 96
+    else:               # uppercase
+        priority -= 38
+
+    return priority
+
+
 def part1():
     file = open('d3.dat', 'r')
     lines = file.readlines()
-    compartmentSimilarities = []
+    compartmentSimilarities = 0
     for line in lines:
         mid = int(len(line)/2)
-        string1 = line[0:(mid-1)]
-        string2 = line[mid:(mid*2-1)]
-        for j in range(mid):
+        string1 = line[0:(mid)]
+        string2 = line[mid:(mid*2)]
+        for letter in string1:
 
-                if(string1[j-1] in string2):
+                if(letter in string2):
 
-                    compartmentSimilarities.append(string1[j])
+                    compartmentSimilarities += getPriority(letter)
                     break
 
-    
     print(compartmentSimilarities)
 #cd .\AoC-2022\   
 #python3 d3.py
 part1()
+
+def part2():
+    file = open('d3.dat', 'r')
+    lines = file.readlines()
+    compartmentSimilarities = 0
