4c70859a820ef30f796587b9f30109bd970fd63d
Matt Collins <mcollins24@hanoverstudents.org>
2022-12-14 12:48:05 -0500

    ndkfl

--- AoC-2020/2020-d2.py
+++ AoC-2020/2020-d2.py
@@ -1,7 +1,13 @@
+#def countLetter(line):
+    
+
 def part1():
     file = open('2020-d2.dat', 'r')
     lines = file.readlines()
     for line in lines:
         line = line.split('-')
+        line = line.split(':')
+
         print(line)
+        #count = countLetter(line)
 part1()
--- AoC-2022/d6.py
+++ AoC-2022/d6.py
@@ -4,12 +4,54 @@ def part1():
     first = ''
     second = ''
     third = ''
-
+    index = 0
     for letter in line:
-        if(letter != third and letter != second and letter != first and first != third and second != third and first != second):
-            print(letter)
+        print(third + second + first + letter)
+        if(letter != third and letter != second and letter != first and first != third and second != third and first != second and index > 3):
+            print(index)
+            break
         third = second
         second = first
         first = letter
+        index += 1
+
+
+part1()
+def part1():
+    file = open('d6.dat', 'r')
+    line = file.readline().strip()
+    first = ''
+    second = ''
+    third = ''
+    fourth = ''
+    fifth = ''
+    sixth = ''
+    seventh = ''
+    eighth = ''
+    ninth = ''
+    tenth = ''
+    eleventh = ''
+    twelth = ''
+    thirteenth = ''
+    index = 0
+    for letter in line:
+        if(letter != third and letter != second and letter != first and first != third and second != third and first != second and index > 13):
+            print(index)
+            break
+        thirteenth = twelth
+        twelth = eleventh
+        eleventh = tenth
+        tenth = ninth
+        ninth = eighth
+        eighth = seventh
+        seventh = sixth
+        sixth = fifth
+        fifth = fourth
+        fourth = third
+        third = second
+        second = first
+        first = letter
+        index += 1
+
 
 part1()
