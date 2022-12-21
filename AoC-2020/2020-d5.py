cb0dfc7bf92e3e66a599dc9a3ba6a64ed230129c
Matt Collins <mcollins24@hanoverstudents.org>
2022-12-21 12:48:34 -0500

    downto4lines

--- AoC-2020/2020-d5.py
+++ AoC-2020/2020-d5.py
@@ -1,8 +1,4 @@
-def part1():
-    file = open('2020-d5.dat', 'r')
-    lines = file.readlines()
-    possibilities = []
-    while True:
-        
-
-    for line in lines:
+import re
+for line in (open('2020-d5.dat', 'r').readlines()):
+    if (re.search(r'^[B]{4}FB', line.strip()[:7]) and re.search(r'^L[R]{1}', line.strip()[7:])):
+        print(line)
