9e947ef627f0af94510cd3af70a809850d30223f
Matt Collins <mcollins24@hanoverstudents.org>
2023-01-12 12:48:07 -0500

    insert

--- SQL/delete.sql
+++ SQL/delete.sql
@@ -29,7 +29,6 @@ Delete
  Where att_stu_id = 'stdX2000001008'
 ;
 
-
 Select *
   From schedules
  Where ssc_stu_id = 'stdX2000001008'
@@ -39,7 +38,6 @@ Delete
   From schedules
  Where ssc_stu_id = 'stdX2000001008'
 ;
-
 
 Select *
   From transcripts
@@ -53,12 +51,69 @@ Delete
 
 -- Scanning for unacompanied children
 
+Select stu_id, stu_last_name, stu_first_name, count(*)
+  From students
+  Left Outer Join attendance ON stu_id = att_stu_id
+ Where att_stu_id IS null
+;
+
+Select stu_id, stu_last_name, stu_first_name
+  From students
+  Left Outer Join attendance ON stu_id = att_stu_id
+ Where stu_grade_level = '12' AND stu_last_name LIKE 'Le%' AND att_stu_id IS null
+;
+
+
 Select *
   From attendance
-  Join students
- Where att_stu_id Not In stu_id
- ;
+  Left Outer Join students ON stu_id = att_stu_id
+  Where stu_id IS null
+;
 
-
-
+Select *
+  From attendance
+  Join students On Not stu_id = att_stu_id
+;
  
+ -- 0 Orphans Detected
+ -- Eliminating Parents to create Orphans
+ 
+ Delete 
+   From students
+  Where stu_last_name LIKE 'Z%'
+;
+
+Delete attendance
+  From attendance
+  Left Outer Join students ON stu_id = att_stu_id
+  Where stu_id IS null
+;
+
+-- IN op
+
+Select * 
+  From students
+ Where stu_skl_id IN(
+Select skl_id 
+  From schools
+ Where skl_name LIKE '%Elementary%'
+ )
+;
+
+-- Pre-termination SELECT statement
+Select *
+  From attendance
+ Where att_stu_id NOT IN(
+ Select stu_id
+   From students
+)
+;
+
+-- Eliminate all Orphans previously created
+Delete
+  From attendance
+ Where att_stu_id NOT IN(
+ Select stu_id
+   From students
+)
+;
---
+++ SQL/insert.sql
@@ -0,0 +1,11 @@
+-- Inserting records into a table
+
+Insert 
+  Into students (stu_id, stu_first_name, stu_last_name, stu_grade_level)
+Values ( 'BTTF2015','Marty', 'McFly', '11')
+;
+
+Select *
+  From students
+ Where stu_id = 'BTTF2015'
+ ;
