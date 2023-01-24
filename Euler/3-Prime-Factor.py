a644c660e1e2f6965fbd2875f7cadbcccbcccc47
Matt Collins <mcollins24@hanoverstudents.org>
2023-01-24 07:54:49 -0500

    semi-final commit

--- Euler/3-Prime-Factor.py
+++ Euler/3-Prime-Factor.py
@@ -1,6 +1,8 @@
+import math
 num = 600851475143
-highestPrimeFactor = 13
-for i in range(num):
+limit = math.floor(math.sqrt(num))
+highestPrimeFactor = 0
+for i in range(limit):
     if (not((i % 2 == 0) or (i % 3 == 0) or (i % 5 == 0))):
         if (num % i == 0):
             highestPrimeFactor = i
