824a161e1f3fa9cab684b2ebf970fe84b3123939
Matt Collins <mcollins24@hanoverstudents.org>
2023-01-26 12:48:44 -0500

    hsdjjdkdfk

--- php/ticket-submit.php
+++ php/ticket-submit.php
@@ -6,17 +6,38 @@
 
     <body>
 
-        <h1>Thank ou for your request.</h1>
+        <h1>Thank you for your request.</h1>
 
         <?php
             extract($_REQUEST);
 
-            echo  " Problem: $problem" .
-                  " Soultion: Unknown" .
-                  " Email: $contact" .
-                  " Priority: $priority" .
-                  " Status: Submitted" 
+            $servername = "localhost";
+            $username = "root";
+            $password = "password";
+            $dbname = "helpdesk";
 
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error){
+                die("Connection Falied: " . $conn->connect_error);
+            }
+
+            $problem = $conn->real_escape_string($problem);
+            $contact = $conn->real_escape_string($contact);
+            
+            
+            $sql = "INSERT INTO tickets (tck_issue, tck_priority, tck_email)" .
+            "VALUES ('$problem', $priority, '$contact')";
+            
+            echo $sql;
+
+
+            $conn->query($sql);
+            
+            // echo  " Problem: $problem" .
+            //       " Soultion: Unknown" .
+            //       " Email: $contact" .
+            //       " Priority: $priority" .
+            //       " Status: Submitted" ;
             ?>
 
         <h3>Your unique ticket id: <span font-style="italic" id="ticketID"></span></h3>
