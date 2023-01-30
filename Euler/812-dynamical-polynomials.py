bd4f7f5dc6a52312fd41839cb3895e4cf30558c8
Matt Collins <mcollins24@hanoverstudents.org>
2023-01-30 12:47:08 -0500

    piuyerwd

--- Euler/812-dynamical-polynomials.py
+++ Euler/812-dynamical-polynomials.py
@@ -1,6 +1,6 @@
-degree = 3
+degree = 5
 mod = 998244353
 num_poly = 1
 for i in range(1, degree + 1):
-    num_poly = (num_poly * (i + degree)) % mod
+    num_poly = (num_poly * (i + degree))
 print(num_poly)
