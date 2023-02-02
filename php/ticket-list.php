4b536186d92f06e6bf0ca8b6a6197172fc0b198d
Matt Collins <mcollins24@hanoverstudents.org>
2023-02-02 12:47:32 -0500

    php additions

--- php/ticket-list.php
+++ php/ticket-list.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?php
     include('library.php');
 ?>
@@ -26,7 +27,10 @@
                 <th>PRIORITY</th>
                 <th>ISSUE</th>
                 <th>EMAIL</th>
+                <th>EDIT</th>
                 <th>DELETE</th>
+                <th>STATUS</th>
+                <th>RESOLUTION</th>
             </tr>
 
         <?php
@@ -42,9 +46,13 @@
                 echo "<td>" . $row['tck_priority'] . "</td>";
                 echo "<td>" . $row['tck_issue'] . "</td>";
                 echo "<td><a href='mailto:" . $row['tck_email'] . "'>" . $row['tck_email'] . "</a></td>";
-                echo "<td><button onclick='deleteTicket(". $row['tck_id'] . ")'>Delete</button></td>";
+                echo "<td><a href='ticket-edit.php?id=". $row['tck_id'] . ")'><i class='fa fa-pencil' aria-hidden='true'></i></button></td>";
+                echo "<td><button onclick='deleteTicket(". $row['tck_id'] . ")'><i class='fa fa-ban' aria-hidden='true'></i></button></td>";
+                echo "<td>" . $row['tck_status'] . "</td>";
+                echo "<td>" . $row['tck_resolution'] . "</td>";
                 echo "</tr>";
             }
+            echo "<a href= 'ticket-form.php'><button><i class='fa fa-plus-circle' aria-hidden='true'></i></button></a>";
 
 
 
