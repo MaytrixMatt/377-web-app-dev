1d9d079ac84ce8f4d7bb2cc3ea5e7852e7e92c1f
Matt Collins <mcollins24@hanoverstudents.org>
2023-02-17 12:00:29 -0500

    freefriday

---
+++ 4-fun-friday/generatearray.js
@@ -0,0 +1,14 @@
+var arr = [];
+
+for(var i = 0; i <= 24; i++){
+  arr.push(i);
+}
+
+for(var i = arr.length - 1; i > 0; i--){
+  var j = Math.floor(Math.random() * (i + 1));
+  var temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+console.log(arr);
