6f58663958498bf9ce4c71d8a4b010084418d38e
Matt <mcollins24@hanoverstudents.org>
2023-02-24 12:46:47 -0500

    error changes

--- 4-fun-friday/doc/errorCalc.js
+++ 4-fun-friday/doc/errorCalc.js
@@ -1,10 +1,4 @@
 function whereTheStuffHappens(a,b,c,d,o){
-    // var a = parseInt(document.getElementById("a").value);
-    // var b = parseInt(document.getElementById("b").value);
-    // var c = parseInt(document.getElementById("c").value);
-    // var d = parseInt(document.getElementById("d").value);
-    // var o = document.getElementById("o");
-    
     if(o == 'A'){
         var cons = a + c;
     }else if(o == 'S'){
@@ -14,18 +8,17 @@ function whereTheStuffHappens(a,b,c,d,o){
     }else if(o == 'D'){
         var cons = a / c;
     }else{
-        //document.getElementById("result").innerHTML = ("Your operation was incorrectly formated");
+        return("Incorrect Format - Operation should be 'A', 'S', 'M', or 'D'");
     }
 
     if(o == 'A' || o == 'S'){
-        error = Math.sqrt((b*b) + (d*d));
+        var error = Math.sqrt((b*b) + (d*d));
+        var p = ''
+        
     }else{
-        error = ((Math.sqrt(((b/a*100)*(b/a*100)) + ((d/c*100)*(d/c*100))))/100)*cons;
+        var error = ((Math.sqrt(((b/a*100)*(b/a*100)) + ((d/c*100)*(d/c*100))))/100);
+        var p = ' %'
     }
-    //document.getElementById("result").innerHTML = (cons + ' ± ' + error);
-    return(cons + ' ± ' + error);
+
+    return(cons + ' ± ' + error + p);
 }
-
-
-//The following command will run as expected, the problem lies within the html intergration
-//console.log(whereTheStuffHappens(207.9,2.1,107.7,.5,'S'));
